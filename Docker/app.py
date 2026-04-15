057500ad30d590a3245eeb8a453ca06c48765ac8
yying1207 <1022949490@qq.com>
2026-04-15 18:20:43 +0800

    Add Docker folder

---
+++ Docker/app.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+color = os.environ.get('APP_COLOR')
+
+@app.route('/')
+def hello():
+    print(color)
+
+#def main():
+    #print(color)
+    #return render_template('hello.html', color=color)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="8080")
